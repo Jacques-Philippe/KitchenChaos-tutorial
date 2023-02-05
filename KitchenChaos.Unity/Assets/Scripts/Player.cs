9aad5e27b9634395eeb47cc1a1f39e6ca958b9c6
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2023-02-04 22:09:20 -0500

    Add collision detection for player movement

--- KitchenChaos.Unity/Assets/Scripts/Player.cs
+++ KitchenChaos.Unity/Assets/Scripts/Player.cs
@@ -34,11 +34,43 @@ namespace KitchenChaosTutorial
             Vector2 movementInput = mGameInput.GetPlayerMovementDirectionNormalized();
 
             Vector3 movementDirection = new Vector3(movementInput.x, 0.0f, movementInput.y);
+            float movementDistance = this.mSpeed * Time.deltaTime;
+
+            if (IsPlayerMovementObstructed(movementDirection: movementDirection, movementDistance: movementDistance))
+            {
+                Vector3 movementDirectionX = new Vector3(movementDirection.x, 0.0f, 0.0f).normalized;
+
+                //if the player can move in X, move in X
+                if (!IsPlayerMovementObstructed(movementDirection: movementDirectionX, movementDistance))
+                {
+                    movementDirection = movementDirectionX;
+                }
+                //else the player cannot move in Z, so try to move in Z
+                else
+                {
+                    Vector3 movementDirectionZ = new Vector3(0.0f, 0.0f, movementDirection.z).normalized;
+                    //if the player can move in Z, move in Z
+                    if (!IsPlayerMovementObstructed(movementDirection: movementDirectionZ, movementDistance: movementDistance))
+                    {
+                        movementDirection = movementDirectionZ;
+                    }
+                }
+            }
 
             this.IsWalking = movementDirection != Vector3.zero;
 
-            this.transform.position += movementDirection * this.mSpeed * Time.deltaTime;
+            this.transform.position += movementDirection * movementDistance;
             this.transform.forward = Vector3.Lerp(this.transform.forward, movementDirection, mRotateSpeed * Time.deltaTime);
+        }
+
+        private bool IsPlayerMovementObstructed(Vector3 movementDirection, float movementDistance)
+        {
+            float playerHeight = 2.0f;
+            float playerRadius = 0.7f;
+
+
+            bool isPlayerObstructed = Physics.CapsuleCast(point1: this.transform.position, point2: this.transform.position + (Vector3.up * playerHeight), radius: playerRadius, direction: movementDirection, maxDistance: movementDistance);
+            return isPlayerObstructed;
         }
     }
 
