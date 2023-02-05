1da0e2c885a597d430c6c8f718671a55b13bb3a2
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2023-02-05 13:12:44 -0500

    Spawn in tomato or cheese to kitchen counter from KitchenObjectSO

--- KitchenChaos.Unity/Assets/Scripts/ClearCounter.cs
+++ KitchenChaos.Unity/Assets/Scripts/ClearCounter.cs
@@ -7,10 +7,20 @@ namespace KitchenChaosTutorial
 
     public class ClearCounter : MonoBehaviour
     {
+        /// <summary>
+        /// A reference to the <see cref="KitchenObjectSO"/> object which should drive this table's functionality
+        /// </summary>
+        [SerializeField] private KitchenObjectSO mKitchenObjectSO;
+        /// <summary>
+        /// A reference to a point located on the counter top
+        /// </summary>
+        [SerializeField] private Transform mCounterTopPoint;
 
         public void Interact()
         {
             Debug.Log("Interact!");
+            Instantiate(mKitchenObjectSO.Prefab, mCounterTopPoint.position, rotation: Quaternion.identity);
+            Debug.Log(mKitchenObjectSO.Name);
         }
     }
 
