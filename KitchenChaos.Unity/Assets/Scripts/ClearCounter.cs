53f16141f121f63a8c4465d1694ea7dcf52a9087
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2023-02-05 14:26:47 -0500

    Associate KitchenObject to Counter on interact

--- KitchenChaos.Unity/Assets/Scripts/ClearCounter.cs
+++ KitchenChaos.Unity/Assets/Scripts/ClearCounter.cs
@@ -16,12 +16,56 @@ namespace KitchenChaosTutorial
         /// </summary>
         [SerializeField] private Transform mCounterTopPoint;
 
+        /// <summary>
+        /// A reference to the kitchen object currently on the counter, if any
+        /// </summary>
+        private KitchenObject mKitchenObject;
+
         public void Interact()
         {
             Debug.Log("Interact!");
-            Instantiate(mKitchenObjectSO.Prefab, mCounterTopPoint.position, rotation: Quaternion.identity);
-            Debug.Log(mKitchenObjectSO.Name);
+            GameObject kitchenGameObj = Instantiate(original: mKitchenObjectSO.Prefab, position: mCounterTopPoint.position, rotation: Quaternion.identity, parent: this.transform);
+            if (kitchenGameObj.TryGetComponent<KitchenObject>(out KitchenObject kitchenObject))
+            {
+                this.mKitchenObject = kitchenObject;
+                kitchenObject.SetClearCounter(this);
+            }
+
         }
+
+        /// <summary>
+        /// A function to set the kitchen object on this counter to the provided <paramref name="newKitchenObject"/>
+        /// </summary>
+        /// <param name="newKitchenObject"></param>
+        public void SetKitchenObject(KitchenObject newKitchenObject)
+        {
+            this.mKitchenObject = newKitchenObject;
+        }
+
+        /// <summary>
+        /// A function to get the kitchen object on this counter
+        /// </summary>
+        /// <param name="newKitchenObject"></param>
+        public KitchenObject GetKitchenObject()
+        {
+            return this.mKitchenObject;
+        }
+
+        public void ClearKitchenObjectFromCounter()
+        {
+            this.mKitchenObject = null;
+        }
+
+        /// <summary>
+        /// Whether there is currently a kitchen object on this counter
+        /// </summary>
+        /// <returns></returns>
+        public bool HasKitchenObject()
+        {
+            return this.mKitchenObject != null;
+        }
+
+
     }
 
 }
