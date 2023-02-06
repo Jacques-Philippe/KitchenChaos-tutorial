de62edef2dee85a218cc7e1f0a50e668120b9680
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2023-02-05 21:32:16 -0500

    Update ContainerCounter with ContainerCounter class

---
+++ KitchenChaos.Unity/Assets/Scripts/ContainerCounter.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KitchenChaosTutorial
+{
+
+    public class ContainerCounter : BaseCounter
+    {
+        /// <summary>
+        /// A reference to the <see cref="KitchenObjectSO"/> object which should drive this table's functionality
+        /// </summary>
+        [SerializeField] private KitchenObjectSO mKitchenObjectSO;
+
+
+        public override void Interact(Player player)
+        {
+            KitchenObject counterKitchenObject = this.GetKitchenObject();
+            KitchenObject playerKitchenObject = player.GetKitchenObject();
+            //if there is no kitchen object on the counter and the player has no kitchen object, spawn it
+            if (counterKitchenObject == null && playerKitchenObject == null)
+            {
+                GameObject kitchenGameObj = Instantiate(original: mKitchenObjectSO.Prefab);
+                if (kitchenGameObj.TryGetComponent<KitchenObject>(out KitchenObject _kitchenObject))
+                {
+                    _kitchenObject.setKitchenObjectParent(this);
+                }
+            }
+            else if (counterKitchenObject == null && playerKitchenObject != null)
+            {
+                playerKitchenObject.setKitchenObjectParent(this);
+            }
+            //else if there is a kitchen object on the counter and the player has no kitchen object, give it to the player
+            else if (counterKitchenObject != null && playerKitchenObject == null)
+            {
+                counterKitchenObject.setKitchenObjectParent(player);
+            }
+
+        }
+    }
+
+}
