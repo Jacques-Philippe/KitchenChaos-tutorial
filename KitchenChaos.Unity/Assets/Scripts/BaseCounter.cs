9bcf49eb4c5508f6e0c40e276f2775fad39194f7
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2023-02-05 21:01:14 -0500

    Create BaseCounter class

---
+++ KitchenChaos.Unity/Assets/Scripts/BaseCounter.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KitchenChaosTutorial
+{
+
+    public abstract class BaseCounter : MonoBehaviour, IKitchenObjectParent
+    {
+        [SerializeField] private Transform mCounterTopPoint;
+
+        private KitchenObject mKitchenObject;
+
+        /// <summary>
+        /// A function to determine how the counter will respond to player interaction
+        /// </summary>
+        /// <param name="player"></param>
+        public abstract void Interact(Player player);
+
+        /// <inheritdoc/>
+        public void ClearKitchenObject()
+        {
+            this.mKitchenObject = null;
+        }
+
+        /// <inheritdoc/>
+        public KitchenObject GetKitchenObject()
+        {
+            return this.mKitchenObject;
+        }
+
+        /// <inheritdoc/>
+        public Transform GetKitchenObjectFollowTransform()
+        {
+            return this.mCounterTopPoint;
+        }
+
+        /// <inheritdoc/>
+        public bool HasKitchenObject()
+        {
+            return this.mKitchenObject != null;
+        }
+
+        /// <inheritdoc/>
+        public void SetKitchenObject(KitchenObject newKitchenObject)
+        {
+            this.mKitchenObject = newKitchenObject;
+        }
+    }
+}
