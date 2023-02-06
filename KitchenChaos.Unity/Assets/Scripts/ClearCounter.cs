4632cce3f6d45dbc1bdcf3200e0976f8ef3398ac
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2023-02-05 21:07:40 -0500

    Make ClearCounter inherit from BaseCounter

--- KitchenChaos.Unity/Assets/Scripts/ClearCounter.cs
+++ KitchenChaos.Unity/Assets/Scripts/ClearCounter.cs
@@ -5,86 +5,35 @@ using UnityEngine;
 namespace KitchenChaosTutorial
 {
 
-    public class ClearCounter : MonoBehaviour, IKitchenObjectParent
+    public class ClearCounter : BaseCounter
     {
         /// <summary>
         /// A reference to the <see cref="KitchenObjectSO"/> object which should drive this table's functionality
         /// </summary>
         [SerializeField] private KitchenObjectSO mKitchenObjectSO;
-        /// <summary>
-        /// A reference to a point located on the counter top
-        /// </summary>
-        [SerializeField] private Transform mCounterTopPoint;
+        
 
-        //TODO DELETE ME
-        [SerializeField] private bool mTesting;
-
-        [SerializeField] private ClearCounter mSecondCounter;
-
-        /// <summary>
-        /// A reference to the kitchen object currently on the counter, if any
-        /// </summary>
-        private KitchenObject mKitchenObject;
-
-        private void Update()
-        {
-            if (this.mTesting && Input.GetKeyDown(KeyCode.T))
-            {
-                //Given the kitchen game object is spawned and associated to this counter
-                //Transfer it to another counter
-                this.mKitchenObject.setKitchenObjectParent(mSecondCounter);
-            }
-        }
-
-        public void Interact(Player player)
+        public override void Interact(Player player)
         {
             Debug.Log("Interact!");
+            KitchenObject kitchenObject = this.GetKitchenObject();
             //if there is no kitchen object, spawn it
-            if (this.mKitchenObject == null)
+            if (kitchenObject == null)
             {
                 GameObject kitchenGameObj = Instantiate(original: mKitchenObjectSO.Prefab);
-                if (kitchenGameObj.TryGetComponent<KitchenObject>(out KitchenObject kitchenObject))
+                if (kitchenGameObj.TryGetComponent<KitchenObject>(out KitchenObject _kitchenObject))
                 {
-                    kitchenObject.setKitchenObjectParent(this);
+                    _kitchenObject.setKitchenObjectParent(this);
                 }
             }
             //else if there is a kitchen object, give it to the player
             else
             {
-                this.mKitchenObject.setKitchenObjectParent(player);
+                kitchenObject.setKitchenObjectParent(player);
             }
 
         }
 
-        /// <inheritdoc/>
-        public Transform GetKitchenObjectFollowTransform()
-        {
-            return this.mCounterTopPoint;
-        }
-
-        /// <inheritdoc/>
-        public void SetKitchenObject(KitchenObject newKitchenObject)
-        {
-            this.mKitchenObject = newKitchenObject;
-        }
-
-        /// <inheritdoc/>
-        public KitchenObject GetKitchenObject()
-        {
-            return this.mKitchenObject;
-        }
-
-        /// <inheritdoc/>
-        public void ClearKitchenObject()
-        {
-            this.mKitchenObject = null;
-        }
-
-        /// <inheritdoc/>
-        public bool HasKitchenObject()
-        {
-            return this.mKitchenObject != null;
-        }
 
 
     }
