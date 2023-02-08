0e28c5a5c2fe96ca50874b5060c4a896b1a53034
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2023-02-08 18:37:43 -0500

    Define StoveCounter prefab

---
+++ KitchenChaos.Unity/Assets/Scripts/StoveCounter.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KitchenChaosTutorial
+{
+    public class StoveCounter : BaseCounter
+    {
+        public override void Interact(Player player)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
