51c1c60f99c8d3836d2e94dda0b21e3c4700bf2e
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2023-02-09 21:13:27 -0500

    Spawn a plate after four seconds

--- KitchenChaos.Unity/Assets/Scripts/PlateCounter.cs
+++ KitchenChaos.Unity/Assets/Scripts/PlateCounter.cs
@@ -7,6 +7,32 @@ namespace KitchenChaosTutorial
 
     public class PlateCounter : BaseCounter
     {
+        [SerializeField] private KitchenObjectSO mPlateKitchenObjectSO;
+
+        /// <summary>
+        /// The timer responsible for spawning plater
+        /// </summary>
+        private float mSpawnTimer = 0.0f;
+        /// <summary>
+        /// The interval between each plate spawn
+        /// </summary>
+        private float mTimeToSpawnPlate = 4.0f;
+
+        //Spawn a plate every four seconds
+        //Unless there are already four plates
+
+        private void Update()
+        {
+            if (mSpawnTimer >= mTimeToSpawnPlate)
+            {
+                KitchenObject.SpawnKitchenObject(kitchenObjectSO: mPlateKitchenObjectSO, this);
+            }
+            else
+            {
+                mSpawnTimer += Time.deltaTime;
+            }
+        }
+
         public override void Interact(Player player)
         {
             throw new System.NotImplementedException();
