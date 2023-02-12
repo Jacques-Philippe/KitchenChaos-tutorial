a39edc71d6cd9b29b2a70ecda62dc67ea0bc2c43
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2023-02-11 19:25:54 -0500

    Player can add to plate from ContainerCounter

--- KitchenChaos.Unity/Assets/Scripts/Counters/ContainerCounter.cs
+++ KitchenChaos.Unity/Assets/Scripts/Counters/ContainerCounter.cs
@@ -13,32 +13,62 @@ namespace KitchenChaosTutorial
         /// </summary>
         [SerializeField] private KitchenObjectSO mKitchenObjectSO;
 
+        /// <summary>
+        /// Event fired for container opened
+        /// </summary>
         public event EventHandler ContainerOpened;
+
 
         public override void Interact(Player player)
         {
             KitchenObject counterKitchenObject = this.GetKitchenObject();
             KitchenObject playerKitchenObject = player.GetKitchenObject();
-            //if there is no kitchen object on the counter and the player has no kitchen object, spawn it
-            if (counterKitchenObject == null && playerKitchenObject == null)
+
+            //if the player has a kitchen object
+            if (player.HasKitchenObject())
             {
-                GameObject kitchenGameObj = Instantiate(original: mKitchenObjectSO.Prefab);
-                if (kitchenGameObj.TryGetComponent<KitchenObject>(out KitchenObject _kitchenObject))
+                //if there is no kitchen object on the counter
+                if (counterKitchenObject == null)
                 {
-                    _kitchenObject.setKitchenObjectParent(player);
-                    this.ContainerOpened?.Invoke(sender: this, e: EventArgs.Empty);
+                    //put the player kithen object onto the counter
+                    playerKitchenObject.setKitchenObjectParent(this);
+                }
+                //if there is a kitchen object on the counter
+                else
+                {
+                    //if the player has a plate
+                    if (playerKitchenObject.TryGetPlate(out PlateKitchenObject plateKitchenObject))
+                    {
+                        //if the ingredient can be added to the plate
+                        if (plateKitchenObject.TryAddIngredient(counterKitchenObject.GetKitchenObjectSO()))
+                        {
+                            counterKitchenObject.DestroySelf();
+                        }
+                    }
                 }
             }
-            else if (counterKitchenObject == null && playerKitchenObject != null)
+            //else if the player doesn't have a kitchen object
+            else
             {
-                playerKitchenObject.setKitchenObjectParent(this);
+                //if there is no kitchen object on the counter
+                if (counterKitchenObject == null)
+                {
+                    //spawn a kitchen object
+                    GameObject kitchenGameObj = Instantiate(original: mKitchenObjectSO.Prefab);
+                    if (kitchenGameObj.TryGetComponent<KitchenObject>(out KitchenObject _kitchenObject))
+                    {
+                        //give it to the player
+                        _kitchenObject.setKitchenObjectParent(player);
+                        this.ContainerOpened?.Invoke(sender: this, e: EventArgs.Empty);
+                    }
+                }
+                //else if there is a kitchen object on the counter
+                else
+                {
+                    //let player pick up counter kitchen object
+                    counterKitchenObject.setKitchenObjectParent(player);
+                }
             }
-            //else if there is a kitchen object on the counter and the player has no kitchen object, give it to the player
-            else if (counterKitchenObject != null && playerKitchenObject == null)
-            {
-                counterKitchenObject.setKitchenObjectParent(player);
-            }
-
         }
     }
 
