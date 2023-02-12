2cbb4fad2934de86d25ca876f959ccde18709b10
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2023-02-11 20:00:51 -0500

    Add more documentation to PlateKitchenObjectVisual

--- KitchenChaos.Unity/Assets/Scripts/PlateKitchenObjectVisual.cs
+++ KitchenChaos.Unity/Assets/Scripts/PlateKitchenObjectVisual.cs
@@ -8,14 +8,29 @@ namespace KitchenChaosTutorial
 
     public class PlateKitchenObjectVisual : MonoBehaviour
     {
+        /// <summary>
+        /// A struct to expose a relationship between the inactive ingredient gameobjects in the plate and the incoming <see cref="KitchenObjectSO"/>
+        /// </summary>
         [Serializable]
         public struct PlateKitchenObjectPrefabPairing
         {
+            /// <summary>
+            /// The gameobject associated to the corresponding KitchenObjectSO
+            /// </summary>
             public GameObject IngredientGameobject;
+            /// <summary>
+            /// The KitchenObjectSO used to identify the correpsonding gameobject
+            /// </summary>
             public KitchenObjectSO IngredientKitchenObjectSO;
         }
 
+        /// <summary>
+        /// A list of KitchenObjectSO-gameobject pairings exposing the relationships between them
+        /// </summary>
         [SerializeField] private List<PlateKitchenObjectPrefabPairing> plateKitchenObjectPrefabPairings;
+        /// <summary>
+        /// A reference to the PlateKitchenObject to subscribe to its events
+        /// </summary>
         [SerializeField] private PlateKitchenObject plateKitchenObject;
 
 
@@ -25,11 +40,20 @@ namespace KitchenChaosTutorial
             plateKitchenObject.OnIngredientAdded += PlateKitchenObject_OnIngredientAdded;
         }
 
+        /// <summary>
+        /// Function to run on <see cref="plateKitchenObject"/> 's for new ingredient added to the plate
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void PlateKitchenObject_OnIngredientAdded(object sender, PlateKitchenObject.IngredientAddedEventArgs e)
         {
             this.Show(kitchenObjectSO: e.AddedIngredient);
         }
 
+        /// <summary>
+        /// Helper to find the gameobject associated to the incoming <paramref name="kitchenObjectSO"/> and activate it
+        /// </summary>
+        /// <param name="kitchenObjectSO"></param>
         private void Show(KitchenObjectSO kitchenObjectSO)
         {
             foreach(var pairing in plateKitchenObjectPrefabPairings)
