68374bd9e4e83cf5dbcd3df0a783772790a34f69
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2023-02-16 22:08:27 -0500

    Slightly refactored DeliveryManager to be more consistent with UI scripts

---
+++ KitchenChaos.Unity/Assets/Scripts/UI/DeliveryDisplayFactoryUI.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace KitchenChaosTutorial
+{
+
+    public class DeliveryDisplayFactoryUI : MonoBehaviour
+    {
+        /// <summary>
+        /// The prefab to be instantiated to UI on new recipe order added or removed
+        /// </summary>
+        [SerializeField] private GameObject templateRecipeUI;
+
+
+        private void Start()
+        {
+            DeliveryManager.Instance.OnOrderAdded += Instance_OnRecipeAdded;
+            DeliveryManager.Instance.OnOrderRemoved += Instance_OnRecipeRemoved;
+
+            this.templateRecipeUI.SetActive(false);
+        }
+
+
+        private void Instance_OnRecipeRemoved(object sender, System.EventArgs e)
+        {
+            this.UpdateUI();
+        }
+
+        private void Instance_OnRecipeAdded(object sender, System.EventArgs e)
+        {
+            this.UpdateUI();
+
+        }
+
+        /// <summary>
+        /// Function to invoke on food order added or removed; updates the UI with the new list of orders
+        /// </summary>
+        private void UpdateUI()
+        {
+            //Clear existing UI, destroy all but the template UI
+            foreach (Transform child in this.transform)
+            {
+                if (child.gameObject != this.templateRecipeUI)
+                {
+                    GameObject.Destroy(child.gameObject);
+                }
+            }
+            //Create new UI
+            foreach (var recipe in DeliveryManager.Instance.GetListOfWaitingRecipes())
+            {
+                GameObject templateInstance = GameObject.Instantiate(original: this.templateRecipeUI, parent: this.transform);
+                if (templateInstance.TryGetComponent<TemplateRecipeUI>(out TemplateRecipeUI templateRecipeUI))
+                {
+                    templateRecipeUI.SetRecipeSO(recipe);
+                }
+                templateInstance.SetActive(true);
+            }
+        }
+
+
+
+    }
+}
--- KitchenChaos.Unity/Assets/Scripts/UI/DeliveryManagerUI.cs
+++ KitchenChaos.Unity/Assets/Scripts/UI/DeliveryManagerUI.cs
@@ -1,65 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace KitchenChaosTutorial
 {
 
     public class DeliveryManagerUI : MonoBehaviour
     {
-        /// <summary>
-        /// The prefab to be instantiated to UI on new recipe order added or removed
-        /// </summary>
-        [SerializeField] private GameObject templateRecipeUI;
-
 
         private void Start()
         {
-            DeliveryManager.Instance.OnOrderAdded += Instance_OnRecipeAdded;
-            DeliveryManager.Instance.OnOrderRemoved += Instance_OnRecipeRemoved;
-
-            this.templateRecipeUI.SetActive(false);
+            GameManager.Instance.OnGameStateChanged += GameManager_OnGameStateChanged;
+            this.Hide();
         }
-
-        private void Instance_OnRecipeRemoved(object sender, System.EventArgs e)
+        private void GameManager_OnGameStateChanged(object sender, GameManager.GameStateChangedEventArgs e)
         {
-            this.UpdateUI();
-        }
-
-        private void Instance_OnRecipeAdded(object sender, System.EventArgs e)
-        {
-            this.UpdateUI();
-
-        }
-
-        /// <summary>
-        /// Function to invoke on food order added or removed; updates the UI with the new list of orders
-        /// </summary>
-        private void UpdateUI()
-        {
-            //Clear existing UI, destroy all but the template UI
-            foreach (Transform child in this.transform)
+            if (e.newState == GameManager.State.GAME_PLAYING)
             {
-                if (child.gameObject != this.templateRecipeUI)
-                {
-                    GameObject.Destroy(child.gameObject);
-                }
-            }
-            //Create new UI
-            foreach (var recipe in DeliveryManager.Instance.GetListOfWaitingRecipes())
-            {
-                GameObject templateInstance = GameObject.Instantiate(original: this.templateRecipeUI, parent: this.transform);
-                if (templateInstance.TryGetComponent<TemplateRecipeUI>(out TemplateRecipeUI templateRecipeUI))
-                {
-                    templateRecipeUI.SetRecipeSO(recipe);
-                }
-                templateInstance.SetActive(true);
+                this.Show();
             }
         }
+        private void Show()
+        {
+            this.gameObject.SetActive(true);
+        }
 
-
+        private void Hide()
+        {
+            this.gameObject.SetActive(false);
+        }
     }
 }
