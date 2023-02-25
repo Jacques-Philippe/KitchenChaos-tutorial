2105bede8df53edc02dbb3bc6ad4e9013a1cd985
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2023-02-25 12:46:41 -0500

    Define StoveCounter Warning icon idle animation

---
+++ KitchenChaos.Unity/Assets/Scripts/StoveCounterWarningUI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KitchenChaosTutorial
+{
+
+    public class StoveCounterWarningUI : MonoBehaviour
+    {
+        //given the state of the StoveCounter
+        //play the warning animation
+        //play the warning sound
+    }
+}
